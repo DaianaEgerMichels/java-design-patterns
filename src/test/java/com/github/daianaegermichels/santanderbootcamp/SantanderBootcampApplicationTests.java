c8deb5f3a0298017df9e1401c8a7759e50f92731
Daiana Michels <daianaem.12@gmail.com>
2023-10-17 20:42:44 -0300

    started project

---
+++ src/test/java/com/github/daianaegermichels/santanderbootcamp/SantanderBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daianaegermichels.santanderbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
